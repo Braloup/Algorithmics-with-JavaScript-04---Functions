9f5b928f3a272934792a0e144a974a541a86cb55
VieuxLoupGris <provocemois@gmail.com>
2019-01-22 16:43:33 +0100

    Write a fonction who take an array argumetn and log average

---
+++ Exercise_4.4.js
@@ -0,0 +1,14 @@
+let toatalNumber = 0;
+let averageNumber;
+
+let average = (arr) => {
+  for (let i = 0; i < myArr.length; i++) {
+
+    toatalNumber += myArr[i];
+    averageNumber = arr.length;
+  }
+  console.log("The average of your array is : " + toatalNumber / averageNumber);
+}
+let myArr = [4, 4, 5, 5];
+
+average(myArr);

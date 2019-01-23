5d64a3c456f8e38eeb2a9657bfd1ac6c4c3e7514
VieuxLoupGris <provocemois@gmail.com>
2019-01-23 11:23:47 +0100

    Return factorial

--- Exercise_4.6.js
+++ Exercise_4.6.js
@@ -1,24 +1,15 @@
 const readLineSync = require("readline-sync");
 
-let numbFact = 0;
-let numMultiply = [];
-let total = 0;
-
-let factorial = () => {
-  numbFact = readLineSync.question("Enter the number you want to factor: ");
+let numbFact = readLineSync.question("Enter the number you want to factor: ");
+let factorial = (numbFact) => {
 
   if (numbFact === 0) { // If numbFact is 0, factor is 1.
-    total = 1;
-  } else if (numbFact > 0){ // Else put every number between 1 and numbFact in a array.
-    while (numbFact > 0) {
-      numMultiply.push(numbFact);
-      numbFact -= 1;
-    }
-    for (var i = 0; i < numMultiply.length; i++) { //Multiply every number between 1 numbFact.
-      total *= numMultiply[i];
-    }
+    return "1";
   }
-  console.log("The factorial is: " + total);
+
+  while (numbFact > 0) { // Else multiply every number between 1 and numbFact.
+    return numbFact * factorial(numbFact - 1);
+  }
 }
 
-factorial();
+console.log(factorial(numbFact));

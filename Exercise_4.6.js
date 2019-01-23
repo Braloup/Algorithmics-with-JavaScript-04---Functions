3dc3761eeaedeeef66066e94edd0ce7cbd1cf0e7
VieuxLoupGris <provocemois@gmail.com>
2019-01-23 10:52:38 +0100

    Number between 1 and factor number run

--- Exercise_4.6.js
+++ Exercise_4.6.js
@@ -2,15 +2,23 @@ const readLineSync = require("readline-sync");
 
 let numbFact = 0;
 let numMultiply = [];
+let total = 0;
 
 let factorial = () => {
   numbFact = readLineSync.question("Enter the number you want to factor: ");
 
-  while (numbFact > 0) {
-    numMultiply.push(numbFact);
-    numbFact -= 1;
+  if (numbFact === 0) { // If numbFact is 0, factor is 1.
+    total = 1;
+  } else if (numbFact > 0){ // Else put every number between 1 and numbFact in a array.
+    while (numbFact > 0) {
+      numMultiply.push(numbFact);
+      numbFact -= 1;
+    }
+    for (var i = 0; i < numMultiply.length; i++) { //Multiply every number between 1 numbFact.
+      total *= numMultiply[i];
+    }
   }
-  console.log(numMultiply);
+  console.log("The factorial is: " + total);
 }
 
 factorial();

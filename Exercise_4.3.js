150abef8cd4c256e355d239530f44d163ef50c60
Emmanuel Goldstein <30625064+VieuxLoupGris@users.noreply.github.com>
2019-01-25 11:34:41 +0100

    comment code

--- Exercise_4.3.js
+++ Exercise_4.3.js
@@ -2,19 +2,17 @@ const readLineSync = require("readline-sync");
 
 let numRandom = readLineSync.question("Please, enter the number of random number you want: ");
 
-
-
-let multiRand = (numRandom) => {
+let multiRand = (numRandom) => { //function retur an array with random number.
 
   let arrRandom = [];
 
-  let rand10 = () => {
+  let rand10 = () => { // Function who generate random number.
 
     let result = Math.floor(Math.random() * Math.floor(10));
 
   }
 
-  for (var i = 0; i < numRandom; i++) {
+  for (var i = 0; i < numRandom; i++) { // Push in a array all of random number the user ask.
 
     let random = rand10();
 

372e5a956c0ed66f083d496de2018e3c0f90b7f7
VieuxLoupGris <provocemois@gmail.com>
2019-01-29 10:23:17 +0100

    Add documentation and comment

--- Exercise_4.1.js
+++ Exercise_4.1.js
@@ -1,5 +1,11 @@
-const readLineSync = require("readline-sync");
+/*
+**Function who ask the user the length and the width of some one rectangle.
+**And return the surface.
+*/
 
+const readLineSync = require("readline-sync");// Call read-line-sync
+
+// Function who return the surface of a rectangle
 let calcSurface = (length, width) => {
   return length * width;
 }

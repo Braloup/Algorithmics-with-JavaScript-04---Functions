721f4a09626747de58ef45b33295fe953c970744
VieuxLoupGris <provocemois@gmail.com>
2019-01-29 10:26:48 +0100

    Add documentation and comment

--- Exercise_4.2.js
+++ Exercise_4.2.js
@@ -1,6 +1,10 @@
+/*
+** Return a random number between 1 and 10.
+*/
+// Function who return a random number.
 let rand10 = () => {
 
-  let result = Math.floor(Math.random() * Math.floor(10));
+  let result = Math.floor(Math.random() * Math.floor(10));// The random number is between 1 and 10.
 
   return result;
 

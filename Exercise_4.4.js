a486f4b24ab3185aa3a91da86ad9bc5688fca9a9
VieuxLoupGris <provocemois@gmail.com>
2019-01-29 11:27:20 +0100

    Add documentation and comment. Make code more elegant

--- Exercise_4.4.js
+++ Exercise_4.4.js
@@ -1,38 +1,56 @@
-const readLineSync = require("readline-sync");
+/*
+ **Create three function.
+ **One for calculate the averange on an array.
+ **One for return the minimum value number of an array.
+ **One for return the maximum value number of an array.
+ **And, last but not leat one more for call this thre one.
+ */
+
+const readLineSync = require("readline-sync"); //Call the module for ask the user.
 
 let toatalNumber = 0;
 let averageNumber = 0;
 let randomArr = [];
 
+//Function who return the average of an an array.
 let average = (arr) => {
+
   for (let i = 0; i < arr.length; i++) {
 
     toatalNumber += arr[i];
     averageNumber = arr.length;
   }
-  console.log("The average of your array is : " + toatalNumber / averageNumber);
+
+  return "The average of your array is : " + (toatalNumber / averageNumber).toFixed(2);
 }
 
+//Function who return the minimum number of an array
 let min = (arr) => {
-  console.log("The minimum element of your aray is: " + Math.min(...arr));
+
+  return "The minimum element of your aray is: " + Math.min(...arr);
 }
 
+// Function who return the maximum number ro an array.
 let max = (arr) => {
-  console.log("The minimum element of your aray is: " + Math.max(...arr));
+
+  return "The minimum element of your aray is: " + Math.max(...arr);
 }
 
+//Function who log the three function above.
 let multiRand = () => {
 
   let arrNumber = readLineSync.question("Enter the number of digits you want to average: ");
 
   while (arrNumber > 0) {
+
     randomArr.push(Math.floor(Math.random() * 100));
     arrNumber -= 1;
   }
+
   console.log("That is your random array between 1 & 100: " + randomArr);
-  average(randomArr);
-  min(randomArr);
-  max(randomArr);
+  console.log(average(randomArr));
+  console.log(min(randomArr));;
+  console.log(max(randomArr));
 }
 
 multiRand();
